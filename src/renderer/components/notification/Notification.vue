c3577329947a40ba74aab8390734827d3db8f429
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-08-08 07:33:28 -0300

    clear dzen2 instances when pomotroid starts

--- src/renderer/components/notification/Notification.vue
+++ src/renderer/components/notification/Notification.vue
@@ -24,22 +24,28 @@ export default {
     },
     timeWork() {
       return this.$store.getters.timeWork
+    },
+    currentRound() {
+      return this.$store.getters.currentRound
     }
   },
 
   methods: {
     callNotification(opts) {
-      if (opts.body.endsWith('break.')) {
-        const child = cp.spawn(
-          'dzen2',
-          ['-p', '-bg', '#8bbe2c', '-m', '-e', 'button1=exit'],
-          {
-            detached: true,
-            stdio: ['ignore', 'ignore', 'ignore']
-          }
-        )
-        child.unref()
+      if (!opts.body.endsWith('break.')) {
+        return
       }
+
+      const child = cp.spawn(
+        'dzen2',
+        ['-p', '-bg', '#8bbe2c', '-m', '-e', 'button1=exit'],
+        {
+          detached: true,
+          stdio: ['ignore', 'ignore', 'ignore']
+        }
+      )
+
+      child.unref()
     },
 
     notifyLongBreak() {
@@ -63,6 +69,34 @@ export default {
         title: 'Break Finished',
         body: `Begin focusing for ${this.timeWork} minutes.`
       })
+    },
+
+    clearDzenNotification() {
+      if (this.currentRound !== 'work') {
+        return
+      }
+
+      const processList = cp
+        .spawnSync('ps', ['-eo', 'pid,command'], { shell: true })
+        .stdout.toString()
+        .split('\n')
+        .slice(1)
+
+      for (const line of processList) {
+        const parts = line.trim().split(' ')
+        const pid = parts[0]
+
+        if (!pid) {
+          continue
+        }
+
+        for (const cmd of parts.slice(1)) {
+          if (cmd.slice(0, 5) === 'dzen2') {
+            cp.execFile('kill', [pid])
+            break
+          }
+        }
+      }
     }
   },
 
@@ -70,12 +104,14 @@ export default {
     EventBus.$on('ready-long-break', this.notifyLongBreak)
     EventBus.$on('ready-short-break', this.notifyShortBreak)
     EventBus.$on('ready-work', this.notifyWork)
+    EventBus.$on('timer-started', this.clearDzenNotification)
   },
 
   beforeDestroy() {
     EventBus.$off('ready-long-break', this.notifyLongBreak)
     EventBus.$off('ready-short-break', this.notifyShortBreak)
     EventBus.$off('ready-work', this.notifyWork)
+    EventBus.$off('timer-started', this.clearDzenNotification)
   }
 }
 </script>
